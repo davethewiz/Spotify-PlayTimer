bf0d2913526abe229942ca5227b9a3fc9119e9c8
Dave <davegershman465@gmail.com>
2018-11-09 23:29:17 -0500

    Dealing With Duration Overshoot
    Section at the bottom added to delete a certain song that can get rid of
    the possible 1-3 minute overshot of playlist duration. This allows
    playlist to now generate within 30 seconds of the specified duration.
    Also cleaned a bit of the structure.

--- PlayTimer.py
+++ PlayTimer.py
@@ -2,7 +2,7 @@ import spotipy
 import spotipy.util as util
 import random
 
-scope = 'user-read-private user-read-playback-state user-modify-playback-state playlist-modify-public'
+scope = 'playlist-modify-public'
 
 username = 'INSERT YOUR USER ID HERE'
 
@@ -31,40 +31,49 @@ playlistIndex = int(input("Enter the number that corresponds to the playlist you
 
 minutes = int(input("How long do you want " + playlists[playlistIndex]['name'] + " to play for? "))
 
-print()
+print() #skip a line for looking clean
 
 latestPlaylist = spotifyObject.user_playlists(username)['items'][0]
 
 newPlayListName = str(minutes) + " Minute " + playlists[playlistIndex]['name'] + " Playlist"
-if (latestPlaylist['name'] != newPlayListName): #test whether or not the latest playlist has been made before
-    spotifyObject.user_playlist_create(username, newPlayListName, public=True) #make one
-    latestPlaylist = spotifyObject.user_playlists(username)['items'][0] #restablishes latestplaylist as the new one
-    playlists = spotifyObject.user_playlists(username)['items'] #restablishes playlist including new one
+#replace = (latestPlaylist['name'] == newPlayListName)
+#print(replace)
+spotifyObject.user_playlist_create(username, newPlayListName, public=True) #make one
+latestPlaylist = spotifyObject.user_playlists(username)['items'][0] #restablishes latestplaylist as the new one
+playlists = spotifyObject.user_playlists(username)['items'] #restablishes playlist including new one
 
-    playlistTracks = spotifyObject.user_playlist(username, playlists[playlistIndex + 1]['id'], fields='tracks,next')['tracks']['items'] #selects a playlist and all of the items in it
-    songs = []
+playlistTracks = spotifyObject.user_playlist(username, playlists[playlistIndex + 1]['id'], fields='tracks,next')['tracks']['items'] #selects a playlist and all of the items in it
+songs = []
 
-    totalDuration = 0
-    wantedLength = minutes * 60000  # 1min = 60,000ms
+totalDuration = 0
+wantedLength = minutes * 60000  # 1min = 60,000ms
 
-    #NOTE: adding tracks to a playlist requires that I access a track's URI
+#NOTE: adding tracks to a playlist requires that I access a track's URI
 
-    while (totalDuration <= wantedLength):
-        i = random.randint(0, len(playlistTracks) - 1)  # choose a random track index
-        uri = playlistTracks[i]['track']['uri'] #set the uri to that random track index
-        while (uri in songs): #if I've already used that track uri
-            i = random.randint(0, len(playlistTracks) - 1)  # choose a new random track
-            uri = playlistTracks[i]['track']['uri']
-        duration = playlistTracks[i]['track']['duration_ms'] #get the duration of the track
-        if totalDuration + duration <= wantedLength:
-            if ('local' not in uri):  # makes sure it doesn't add any local files (URIs don't exist for them)
-                totalDuration += duration
-                songs.append(uri)
-        else:
+while (totalDuration <= wantedLength):
+    i = random.randint(0, len(playlistTracks) - 1)  # choose a random track index
+    uri = playlistTracks[i]['track']['uri'] #set the uri to that random track index
+    while (uri in songs): #if I've already used that track uri
+        i = random.randint(0, len(playlistTracks) - 1)  # choose a new random track
+        uri = playlistTracks[i]['track']['uri']
+    duration = playlistTracks[i]['track']['duration_ms'] #get the duration of the track
+    if ('local' not in uri):  # makes sure it doesn't add any local files (URIs don't exist for them)
+        totalDuration += duration
+        songs.append(uri)
+
+if len(songs) > 0:
+    spotifyObject.user_playlist_add_tracks(username, latestPlaylist['id'], songs) #populate the new playlist
+
+    #deal with overshot:
+    latestPlaylistTracks = spotifyObject.user_playlist(username, latestPlaylist['id'], fields='tracks,next')['tracks']['items'] #get new playlist tracks
+    overShot = totalDuration - wantedLength + 30000 #calculate playlist overshot of duration
+    for track in latestPlaylistTracks: #iterate through all new playlist tracks
+        duration = track['track']['duration_ms']
+        uri = track['track']['uri']
+        if duration < overShot and duration > overShot-30000: #if track found is shorter than the overshot but longer than 30 seconds less than the overshot
+            totalDuration -= duration
+            spotifyObject.user_playlist_remove_all_occurrences_of_tracks(username, latestPlaylist['id'], [uri]) #get rid of it (only one occurance)
             break
+    overShot = totalDuration - wantedLength
 
-    if len(songs) > 0:
-        spotifyObject.user_playlist_add_tracks(username, latestPlaylist['id'], songs) #adds all songs to new playlist
-        print(newPlayListName + " playlist successfully created!")
-else:
-    print(newPlayListName + " playlist already exists!")
+    print(newPlayListName + " playlist successfully created! (Duration " + str(overShot/1000) + " seconds off)")
